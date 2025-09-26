db94fe6e23d37c2d703d1c9828c66d790b03b2b7
ht-156 <github_proton1@outlook.com>
2025-09-26 16:53:49 +0300

    Update dump_pdcclientinfo.py

--- dump_pdcclientinfo.py
+++ dump_pdcclientinfo.py
@@ -3,10 +3,8 @@
 
 import idaapi
 import idc
+import ida_bytes
 
-
-
-  
 
 def get_static_client_count(cfunc):
    
@@ -24,11 +22,11 @@ def get_static_client_count(cfunc):
             if i.op == idaapi.cot_uge or i.op == idaapi.cot_ult:
                 try:
                     if i.y.op == idaapi.cot_num:
-                        print "cot_num found %X\r\n" % (i.y.n._value)
+                        print(f"cot_num found {hex(i.y.n._value)}")
                         self.result_client_count = i.y.n._value
 
                 except Exception:
-                    print "exception in visit_expr\r\n"
+                    print(f"exception in visit_expr")
 
             return 0       
    
@@ -40,31 +38,31 @@ def get_static_client_count(cfunc):
 def get_client_info_count():
     
     func_ea = idaapi.get_name_ea(BADADDR, "PdcValidateClient")
-    print "PdcValidateClient address %LX" % (func_ea)
+    print(f"PdcValidateClient address {hex(func_ea)}")
     cfunc = idaapi.decompile(func_ea)
     res = get_static_client_count(cfunc)
     
     return res
 
 
-
 pdc_client_info = idaapi.get_name_ea(BADADDR, "PdcClientInfo")
-print "PdcClientInfo address 0x%LX" % pdc_client_info
+print(f"PdcClientInfo address {hex(pdc_client_info)}")
 
 client_count = get_client_info_count()
-print "PdcClientInfo array size %X\r\n" % (client_count)
+print(f"PdcClientInfo array size {client_count}")
 client_index = 0
 
 while client_index < client_count:
     
     client_info = pdc_client_info + client_index * 0x18
-    flags = idc.Dword(client_info)
-    name_ea = idc.Qword(client_info + 0x10)
-    client_name = idc.GetString(name_ea, -1, idc.GetStringType(name_ea))
+    flags = ida_bytes.get_dword(client_info)
+    name_ea = ida_bytes.get_qword(client_info + 0x10)
+    client_name_bytes = ida_bytes.get_strlit_contents(name_ea, -1, idc.get_str_type(name_ea))
+    client_name = client_name_bytes.decode('utf-8')
     
     mode = "UserMode"
     if flags & 0x200:
         mode = "KernelMode"
     
-    print "ClientIndex 0x%X: flags %X (%s) Client name \"%s\"" % (client_index, flags, mode, client_name)
+    print(f"ClientIndex {hex(client_index)}: flags {hex(flags)} ({mode}) Client name \"{client_name}\"")
     client_index = client_index + 1
